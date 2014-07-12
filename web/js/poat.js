9670456db58a40695edb94d133d3dd1e97287cdf
Matt Weston <matt.weston@navigo.com.au>
2014-07-12 20:42:29 +1000

    psst... follow me

--- web/js/poat.js
+++ web/js/poat.js
@@ -1,36 +1,27 @@
 var global = {
-  id : 'poat',
-  map : '',
-  geocoder : '',
+  id : "poat",
+  map : "",
+  geocoder : "",
+  apiurl: "http://54.79.38.93/PeeOnATree-Server/api",
   clusterer: null,
+  gpsTimer: null,
+  gpsRefresh: 10000,
   clusterOpts: {gridSize: 100, maxZoom: 18, styles: [
-    {
-      textColor: 'white',
-      url: 'icons/scluster.png',
-      height: 32,
-      width: 32
-    },
-    {
-      textColor: 'white',
-      url: 'icons/mcluster.png',
-      height: 48,
-      width: 48
-    },
-    {
-      textColor: 'white',
-      url: 'icons/lcluster.png',
-      height: 64,
-      width: 64
-    }
+    { textColor: "white", url: "icons/scluster.png", height: 48, width: 48 },
+    { textColor: "white", url: "icons/mcluster.png", height: 48, width: 48 },
+    { textColor: "white", url: "icons/lcluster.png", height: 48, width: 48 }
   ]},
   minZoom: 16,
-  icons : {tree: "icons/tree.png", treehover: "icons/tree-hover.png", treeselected: "icons/tree-selected.png"},
+  icons : {
+    tree: "icons/tree.png", treehover: "icons/tree-hover.png", treeselected: "icons/tree-selected.png"
+  },
   default : new google.maps.LatLng(-37.5652504, 143.8567112)
 };
 
 function initialize() {
   mapInit();
   searchInit();
+  followInit();
 }
 
 function searchInit() {
@@ -39,42 +30,29 @@ function searchInit() {
   });
 }
 
+function followInit() {
+  $("#follow").attr("title","Location from GPS");
+  $("#follow").click(function() {
+    if($(this).hasClass("active")) {
+      if(global.gpsTimer) clearInterval(global.gpsTimer);
+      $(this).removeClass("active");
+    } else {
+      $(this).addClass("active");
+      console.log("pssst... follow me")
+      geolocate();
+      global.gpsTimer = setInterval(geolocate, global.gpsRefresh);
+    }
+  });
+}
+
 function mapInit() {
   var mapStyles = [
-    {
-      "stylers": [
-        { "visibility": "on" },
-        { "invert_lightness": true }
-      ]
-    },{
-      "featureType": "poi",
-      "stylers": [
-        { "visibility": "off" }
-      ]
-    },{
-      "featureType": "road",
-      "stylers": [
-        { "visibility": "simplified" },
-        { "hue": "#ff9100" }
-      ]
-    },{
-      "featureType": "transit",
-      "stylers": [
-        { "visibility": "off" }
-      ]
-    },{
-      "featureType": "water",
-      "elementType": "labels.text.fill",
-      "stylers": [
-        { "invert_lightness": true },
-        { "visibility": "on" }
-      ]
-    },{
-      "featureType": "water",
-      "stylers": [
-        { "invert_lightness": true }
-      ]
-    }
+    {"stylers": [{ "invert_lightness": true }]},
+    {"featureType": "road", "stylers": [{ "visibility": "simplified" },{ "hue": "#ff9100" }]},
+    {"featureType": "water","stylers": [{ "invert_lightness": true }]},
+    {"featureType": "transit", "stylers": [{ "visibility": "off" }]},
+    {"featureType": "poi", "stylers": [{ "visibility": "off" }]},
+    {"featureType": "water", "elementType": "labels.text.fill"}
   ];
 
   var mapOptions = {
@@ -88,14 +66,14 @@ function mapInit() {
   };
 
   // Create map and geocoder objects
-  global.map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+  global.map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
   global.geocoder = new google.maps.Geocoder();
 
   // Map events
   google.maps.event.addListener(global.map, "idle", updateMap);
 
   // Style Google Map
-  var styledMapOptions = { name: 'POAT' };
+  var styledMapOptions = { name: "POAT" };
   var customMapType = new google.maps.StyledMapType(mapStyles, styledMapOptions);
   global.map.mapTypes.set(global.id, customMapType);
 }
@@ -114,35 +92,32 @@ function treeMarker(tree) {
   var mPosition = new google.maps.LatLng(tree.latitude,tree.longitude);
   var marker = new google.maps.Marker({
     position: mPosition,
-    title: tree.trid,
+    id: tree.trid,
     icon: scaledIcon(global.icons.tree, 29, 32)
-  })
-
-  // set all markers not selected
+  });
   marker.selected = false;
-
-  // tree event handlers
   registerTreeMarkerEvents(marker);
-
   return marker;
 }
 
 function registerTreeMarkerEvents(marker) {
   // tree events
-  google.maps.event.addListener(marker, 'mouseover', function () {
+  google.maps.event.addListener(marker, "mouseover", function () {
     if(this.selected == false)
       this.setIcon(scaledIcon(global.icons.treehover, 29, 32));
   });
 
-  google.maps.event.addListener(marker, 'mouseout', function () {
+  google.maps.event.addListener(marker, "mouseout", function () {
     if(this.selected == false)
       this.setIcon(scaledIcon(global.icons.tree, 29, 32));
   });
 
-  google.maps.event.addListener(marker, 'click', function () {
+  google.maps.event.addListener(marker, "click", function () {
     if(this.selected == false) {
       this.setIcon(scaledIcon(global.icons.treeselected, 29, 32));
       this.selected = true;
+      // todo: do something with id (like lookup tree info api)
+      console.log(marker.id);
     } else {
       this.selected = false;
       this.setIcon(scaledIcon(global.icons.tree, 29, 32));
@@ -152,7 +127,6 @@ function registerTreeMarkerEvents(marker) {
 
 function updateMap() {
   var url = buildUrl();
-  console.log(url);
   $.getJSON(url, function(trees) {
     var markers = [];
     $.each(trees, function(i, tree) {
@@ -168,7 +142,7 @@ function buildUrl(lnglat, radius) {
   var lnglat = global.map.getCenter();
   var zoom = global.map.getZoom();
   var r = mapRadius(zoom);
-  var url = "http://54.79.38.93/PeeOnATree-Server/api/trees/bylocation/{0},{1},{2}";
+  var url = global.apiurl+"/trees/bylocation/{0},{1},{2}";
   return url.replace("{0}",lnglat.k).replace("{1}",lnglat.B).replace("{2}",r);
 }
 
@@ -193,30 +167,31 @@ function mapRadius(zoom) {
 
 function search() {
   var address = $("#search").val();
-  global.geocoder.geocode( { 'address': address}, function(results, status) {
+  global.geocoder.geocode( { "address": address}, function(results, status) {
     if (status == google.maps.GeocoderStatus.OK) {
       global.map.setCenter(results[0].geometry.location);
-      console.log('Location found: ' + results[0].formatted_address);
+      console.log("Location found: " + results[0].formatted_address);
     } else if(status == google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
       console.log("Sorry hit Google's API query limit. Try again another day");
     } else if(status == google.maps.GeocoderStatus.ZERO_RESULTS) {
-      console.log('Sorry no results found for that location');
+      console.log("Sorry no results found for that location");
     } else {
-      console.log('Geocode was not successful for the following reason: ' + status);
+      console.log("Geocode was not successful for the following reason: " + status);
     }
   });
 }
 
-function findme() {
+function geolocate() {
   if(navigator.geolocation) {
+    $("#follow").addClass("polling");
     navigator.geolocation.getCurrentPosition(function(position) {
       var latlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
       global.map.setCenter(latlng);
+      $("#follow").removeClass("polling");
     });
   }
 }
 
 $(document).ready(function() {
   initialize();
-  findme();
 });
