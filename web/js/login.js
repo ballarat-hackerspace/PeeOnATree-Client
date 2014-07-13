b0a2a7d9b7b6af4428388ce9e8692fb76fc14f12
Matt Weston <matt.weston@navigo.com.au>
2014-07-13 10:37:26 +1000

    add login js

---
+++ web/js/login.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  $("#loginform").submit(function() {
+    var $form = $(this);
+    var url = $form.attr("action");
+    console.log($form.serialize());
+    $.ajax({
+           type: "POST",
+           url: url,
+           data: $form.serialize(),
+           success: login,
+           failure: fail
+         });
+    return false;
+  });
+});
+
+function login(data) {
+  console.log(data);
+  if(data == "true")
+    pass();
+  else
+    fail();
+}
+
+function fail() {
+  //TODO: put up failed login stuffs.
+  console.log("you shall not pass!");
+}
+
+function pass() {
+  console.log("passed");
+  window.location.href = './app.html';
+}
