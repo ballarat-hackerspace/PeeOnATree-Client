d8f986d55a750938fe6e12da3e6c568a6d48e25f
firnsy <firnsy@gmail.com>
2014-07-13 11:03:47 +1000

    added: updated splash screen.

--- web/js/splash.js
+++ web/js/splash.js
@@ -1,12 +1,57 @@
 function initialize() {
-  $.getJSON({
-    url : "http://54.79.38.93/PeeOnATree-Server/api/user/status"
-  }).then(function(data) {
-    console.log(data.access);
-    //userData = dataReturned;
-  })
+  setTimeout(
+    function() {
+      $.ajax({
+        type: "GET",
+        url: "http://54.79.38.93/PeeOnATree-Server/api/user/status",
+        success: function(data, status, xhr) {
+          window.location.href = './app.html';
+        },
+        error: function(xhr, status, e) {
+          window.location.href = './login.html';
+        }
+      });
+    }, 2000
+  );
+}
+
+function mock() {
+  setTimeout(
+    function() {
+      $('#loader').text('Planting trees ...').fadeIn('slow').delay(500).fadeOut('slow');
+      mock2();
+    }, 2000
+  );
+}
+
+function mock2() {
+  setTimeout(
+    function() {
+      $('#loader').text('Drinking water ...').fadeIn('slow').delay(500).fadeOut('slow');
+      mock3();
+    }, 2000
+  );
+}
+
+function mock3() {
+  setTimeout(
+    function() {
+      $('#loader').text('Checking vicinity ...').fadeIn('slow').delay(500).fadeOut('slow');
+      mock4();
+    }, 2000
+  );
+}
+
+function mock4() {
+  setTimeout(
+    function() {
+      $('#loader').text('Zipping up ...').fadeIn('slow').delay(500).fadeOut('slow');
+      initialize();
+    }, 2000
+  );
 }
 
 $(document).ready(function() {
-  initialize();
+  $('#loader').hide();
+  mock();
 });
