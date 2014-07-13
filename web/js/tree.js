46721c17f86033955d57f56f980652893133359b
Stuart Clark <stu@rtclark.net>
2014-07-13 12:44:54 +1000

    Some more Tree stuff (styles, and initial functionality).

--- web/js/tree.js
+++ web/js/tree.js
@@ -1,14 +1,25 @@
 function initialize() {
   tree = location.search.slice(1);
 
-  $.getJSON({
-    url : "http://54.79.38.93/PeeOnATree-Server/api/tree/" . tree
-  }).then(function(data) {
-    console.log(data);
-    //userData = dataReturned;
-  })
+  $.getJSON("http://54.79.38.93/PeeOnATree-Server/api/tree/" + tree,
+    function (data) {
+      console.log(data);
+      if (data[0].pic) {
+        $('.jumbotron').append('<img src="http://54.79.38.93/PeeOnATree-Server/api/tree/' + tree + '/pic">');
+      }
+    });
+
+  $.getJSON("http://54.79.38.93/PeeOnATree-Server/api/tree/" + tree + "/history",
+    function (data) {
+      $.each(data, function(index, mark) {
+        console.log(mark.uid);
+//        $.getJSON("http://54.79.38.93/PeeOnATree-Server/api/user/" + mark. + "/history",
+        $('.history ul').append('<li>Test</li>');
+      });
+    });
+;
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
   initialize();
 });
